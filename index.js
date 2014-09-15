874d4e1bde6effb41fcb3fb7dfea1358d8b0c7a3
Andy Hu <huyifan@vip.sina.com>
2014-09-15 18:37:15 +0800

    update

--- index.js
+++ index.js
@@ -22,42 +22,39 @@ module.exports = function DiskStore(options) {
 
   var log = options.log || function _noOpLog() {};
 
-  var adapter = {
-
-    rm: function(fd, cb) {
-      return fsx.unlink(fd, function(err) {
-        // Ignore "doesn't exist" errors
-        if (err && (typeof err !== 'object' || err.code !== 'ENOENT')) {
-          return cb(err);
-        } else return cb();
-      });
-    },
-
-    ls: function(dirpath, cb) {
-      return fsx.readdir(dirpath, function (err, files){
-        if (err) return cb(err);
-        files = _.reduce(_.isArray(files)?files:[], function (m, filename){
-          var fd = path.join(dirpath,filename);
-          m.push(fd);
-          return m;
-        }, []);
-        cb(null, files);
-      });
-    },
-
-    read: function(fd, cb) {
-      if (cb) {
-        return fsx.readFile(fd, cb);
-      } else {
-        return fsx.createReadStream(fd);
-      }
-    }
-
+  var adapter = {};
+  adapter.rm = function(fd, cb) {
+    return fsx.unlink(fd, function(err) {
+      // Ignore "doesn't exist" errors
+      if (err && (typeof err !== 'object' || err.code !== 'ENOENT')) {
+        return cb(err);
+      } else return cb();
+    });
   };
 
-  adapter.receive = function (options){
-    return r_buildDiskReceiverStream(options);
-  }
+  adapter.ls = function(dirpath, cb) {
+    return fsx.readdir(dirpath, function (err, files){
+      if (err) return cb(err);
+      files = _.reduce(_.isArray(files)?files:[], function (m, filename){
+        var fd = path.join(dirpath,filename);
+        m.push(fd);
+        return m;
+      }, []);
+      cb(null, files);
+    });
+  };
+
+  adapter.read = function(fd, cb) {
+    if (cb) {
+      return fsx.readFile(fd, cb);
+    } else {
+      return fsx.createReadStream(fd);
+    }
+  };
+
+  adapter.receive = function(options) {
+    return r_buildDiskReceiverStream(options, adapter);
+  };
 
   return adapter;
 };
